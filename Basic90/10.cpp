04e96bdf83f7796afbd5d4ef2dfc952d3ec4dc7b
kamekoko <kameko.jpn17@gmail.com>
2022-03-30 09:07:57 +0900

    idk

---
+++ Basic90/10.cpp
@@ -0,0 +1,13 @@
+// https://twitter.com/e869120/status/1380290146340245505/photo/2
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main() {
+  cin >> n;
+  int c[n];
+  c[0] = 1;
+  cout << c[0] << endl;
+}
